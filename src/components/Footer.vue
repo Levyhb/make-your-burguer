8ed7719e19b7d3bf0b6adf52c95e3ef029190496
Levy Bezerra <levybholanda@gmail.com>
2023-03-17 10:53:50 -0300

    style: footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-  <p>Footer</p>
+  <footer id="footer">
+    <p>Make Your Burger &copy; 2023</p>
+  </footer>
 </template>
 
 <script>
@@ -8,5 +10,17 @@
   }
 </script>
 
-<style>
+<style scoped>
+  #footer{
+    height: 10vh;
+    background-color: #222;
+    border-top: 4px solid #111;
+    position: fixed;
+    color: #FCBA03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    bottom: 0%;
+  }
 </style>
