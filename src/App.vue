453fc0d82a86ff6d850f8104336a8ad9e92095a2
Levy Bezerra <levybholanda@gmail.com>
2023-03-17 10:47:51 -0300

    style: navbar and app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Navbar />
+  <Navbar :logo="logo_src" :alt="app_name"/>
   <router-view/>
   <Footer />
 </template>
@@ -9,10 +9,33 @@ import Footer from './components/Footer.vue';
 import Navbar from './components/Navbar.vue';
 
   export default {
-    components: { Navbar, Footer }
+    components: { Navbar, Footer },
+    data () {
+      return {
+        logo_src: '/img/logo.png',
+        app_name: 'Make Your Burguer'
+      }
+    }
 }
 </script>
 
 <style>
+  * {
+    font-family: helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
 </style>
