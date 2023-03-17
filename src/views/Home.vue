53a14908555abfcc40e401bf412bb7364df48525
Levy Bezerra <levybholanda@gmail.com>
2023-03-17 19:03:01 -0300

    feat: BurgerForm component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,20 @@
   <Banner />
   <div class="main-container">
     <h1>Monte o seu Burger</h1>
-    <p></p>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
 import Banner from '@/components/Banner.vue';
+import BurgerForm from '@/components/BurgerForm.vue';
 import Navbar from '@/components/Navbar.vue';
 
   export default {
-    components: { Navbar, Banner }
+    components: { Navbar, Banner, BurgerForm }
 }
 </script>
+
+<style scoped>
+  
+</style>
